33e5f97f95e121734699a3e3f13ed8ffd10f7fbc
Chanayut Pharathong <chanayut.05375@gmail.com>
2025-06-12 00:31:25 +0700

    static setup injection

--- lib/login_locator.dart
+++ lib/login_locator.dart
@@ -2,7 +2,7 @@ import 'package:fake_login/bloc/login_bloc/login_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 class LoginLocator {
-  void setup() {
+  static void setup() {
     GetIt.I.registerLazySingleton<LoginBloc>(() => LoginBloc(
           baseUrl: GetIt.I.get(),
           dio: GetIt.I.get(),
